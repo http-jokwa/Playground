807d541bbf5f9491dd72c9dae8e2591a79dbcc1a
Joschua Ordonio <115168886+http-jokwa@users.noreply.github.com>
2022-11-02 09:23:48 +0800

    Create SecToMins.java

---
+++ SecToMins.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class SecToMins {
+
+	public static void main(String[] args) {
+	
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter the estimated seconds : ");
+		int num = in.nextInt();
+
+		int min = (num / 60)%60;
+
+		System.out.println("Minutes: " + min);
+		in.close();
+	}
+
+}
